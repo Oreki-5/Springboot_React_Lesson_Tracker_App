a75724159d1fe39ab096e6317c21c7b1b60687aa
Oreki-5 <adityacoc1602@gmail.com>
2026-02-26 14:01:19 +0530

    added boh folders

---
+++ Lesson_Tracker/src/main/java/com/Oreki/Lesson_Tracker/LessonTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.Oreki.Lesson_Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonTrackerApplication.class, args);
+	}
+
+}
---
+++ Lesson_Tracker/src/test/java/com/Oreki/Lesson_Tracker/LessonTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Oreki.Lesson_Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
